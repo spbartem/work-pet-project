4fd4b8b3d9d740bc0a44200b4b7c5e487324c5b9
spbartem <spbartemm@gmail.com>
2025-04-04 11:11:35 +0300

    edit cors config

--- backend/src/main/java/ru/fkr/workpetproject/config/CorsConfig.java
+++ backend/src/main/java/ru/fkr/workpetproject/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://10.30.10.53:3000")
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
